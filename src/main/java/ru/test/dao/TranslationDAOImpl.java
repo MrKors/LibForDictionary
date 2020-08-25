0e7c72221602b22150af91316aff62384fbc23e0
MrKors <mrkorsar@mail.ru>
2020-08-25 10:51:02 +0300

    implements methods: updateTranslation, findByNameAndWord

--- src/main/java/ru/test/dao/TranslationDAOImpl.java
+++ src/main/java/ru/test/dao/TranslationDAOImpl.java
@@ -2,9 +2,11 @@ package ru.test.dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.test.model.Translation;
+import ru.test.model.Word;
 
 import java.util.List;
 
@@ -19,7 +21,12 @@ public class TranslationDAOImpl implements TranslationDAO{
 
     @Override
     public void addTranslation(Translation translation) {
-        sessionFactory.getCurrentSession().saveOrUpdate(translation);
+        sessionFactory.getCurrentSession().save(translation);
+    }
+
+    @Override
+    public void updateTranslation(Translation translation) {
+        sessionFactory.getCurrentSession().update(translation);
     }
 
     @Override
@@ -30,6 +37,16 @@ public class TranslationDAOImpl implements TranslationDAO{
     @Override
     public Translation findById(long id) {
         return sessionFactory.getCurrentSession().get(Translation.class, id);
+    }
+
+    @Override
+    public Translation findByNameAndWord(String name, Word word) {
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("FROM Translation t WHERE t.translation = :name and t.word = :word");
+        query.setParameter("name", name);
+        query.setParameter("word", word);
+
+        return (Translation) query.uniqueResult();
     }
 
     @Override

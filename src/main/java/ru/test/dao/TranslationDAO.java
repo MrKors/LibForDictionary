da3033cb7502e781f43e2d79f1be34e9b65c0f47
MrKors <mrkorsar@mail.ru>
2020-08-25 10:43:38 +0300

    add methods: updateTranslation, findByNameAndWord

--- src/main/java/ru/test/dao/TranslationDAO.java
+++ src/main/java/ru/test/dao/TranslationDAO.java
@@ -1,6 +1,7 @@
 package ru.test.dao;
 
 import ru.test.model.Translation;
+import ru.test.model.Word;
 
 import java.util.List;
 
@@ -8,9 +9,13 @@ public interface TranslationDAO {
 
     void addTranslation (Translation translation);
 
+    void updateTranslation (Translation translation);
+
     void deleteTranslation(Translation translation);
 
     Translation findById (long id);
+
+    Translation findByNameAndWord (String name, Word word);
 
     List<Translation> showTranslations ();
 }

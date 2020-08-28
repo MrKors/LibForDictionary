ec5bda1b7628c0b8533a420298c64062d054cf26
MrKors <tdbkorsar@gmail.com>
2020-08-28 19:20:56 +0300

    add dictionary id to url

--- src/main/webapp/resources/static/js/search-autocomplete.js
+++ src/main/webapp/resources/static/js/search-autocomplete.js
@@ -1,16 +1,31 @@
 $(document).ready(function () {
-    $('#search').autocomplete({
-        source: "/words/search"
-        //     function (request, response) {
-        //     $.ajax({
-        //         url: "/words/search",
-        //         dataType: 'json',
-        //         data: {term: request.term},
-        //         success: function (data) {
-        //             console.log(data);
-        //
-        //         }
-        //     })
-        // }
+    $('#filterDictionary').on('change', function () {
+        var id = $(this).val();
+        console.log(id);
+
+        $('#search').autocomplete({
+
+            source: '/words/search/' + id,
+            //     function (request, response) {
+            //     $.getJSON('/words/search', {
+            //         term: request.term,
+            //         id: $('#id').val()
+            //     }, response);
+            // },
+            //     function (request, response) {
+            //     $.ajax({
+            //         url: "/words/search",
+            //         dataType: 'json',
+            //         data: {
+            //             term: request.term,
+            //             id: $('#id').val()
+            //         },
+            //         success: function (data) {
+            //             console.log(data);
+            //             response (data);
+            //         }
+            //     })
+            // }
+        })
     })
 })

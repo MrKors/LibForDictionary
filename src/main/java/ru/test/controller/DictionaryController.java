65724102dd321e9b18e6991b6af69b9c219e7ad8
MrKors <tdbkorsar@gmail.com>
2020-09-01 17:54:06 +0300

    add @Valid, BindingResult to method (createDictionary) signature

--- src/main/java/ru/test/controller/DictionaryController.java
+++ src/main/java/ru/test/controller/DictionaryController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.test.model.Criteria;
 import ru.test.model.Dictionary;
 import ru.test.service.DictionaryService;
+
+import javax.validation.Valid;
 
 @Controller
 public class DictionaryController {
@@ -29,8 +32,15 @@ public class DictionaryController {
     }
 
     @RequestMapping (value = "/dictionary/create", method = RequestMethod.POST)
-    public String createDictionary (@ModelAttribute("dictionary") Dictionary dictionary) {
-        dictionaryService.addDictionary(dictionary);
-        return "redirect:/create-dictionary";
+    public String createDictionary (@ModelAttribute("dictionary") @Valid Dictionary dictionary, BindingResult bindingResult, Model model) {
+        model.addAttribute("criteria", new Criteria());
+
+        if (!bindingResult.hasErrors()) {
+            dictionaryService.addDictionary(dictionary);
+            return "redirect:/words-list";
+        }
+        else {
+            return "create-dictionary";
+        }
     }
 }
